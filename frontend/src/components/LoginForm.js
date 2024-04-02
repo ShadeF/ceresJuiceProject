45957424cdf32d71cbd0bf2d60dd57f885dcf5ed
ShadeF <samanthashade83@gmail.com>
2024-04-02 14:32:35 +0300

    Link update

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -15,7 +15,7 @@ function LoginForm() {
       alert("Login successful!"); // Replace with your alert or notification component
       // Redirect or perform any other actions after successful login
       //Just testing something
-      window.location.href = "/branch-selection";
+      window.location.href = "/shop";
     } else {
       // Failed login
       alert("Invalid username or password"); // Replace with your alert or notification component
